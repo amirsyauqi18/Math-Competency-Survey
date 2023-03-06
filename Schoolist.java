8ae3f425d4705b765ba33e82ad8b564261baf8e0
amirsyauqi18 <126445356+amirsyauqi18@users.noreply.github.com>
2023-03-06 20:14:30 +0800

    Create Schoolist.java

---
+++ Schoolist.java
@@ -0,0 +1,20 @@
+public class schoolList{
+    //attribute
+    private School school[] = new School[10];
+    private int currnum;
+    
+    //operation
+    public void addSchool(School sc){
+        
+        
+    }
+   
+public static void main(String args[]) {
+    SchoolList moelist = new SchoolList();
+    moeList.addSchool(new School());
+}
+    
+    
+    
+}
+
